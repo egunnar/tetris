acb93765a54cca4342d35ec09205ce688929bc5b
Erik Gartz <eegunnar@yahoo.com>
2010-05-26 23:34:49 -0400

    can now display the new block
    but doesn't move the block yet. only shows it at the top of
    the screen

--- tetris_main.s
+++ tetris_main.s
@@ -10,6 +10,42 @@
 # game play is 
 # screen 16 wide 28 high
 .section .data
+block0_x:
+.int 0, 0, 0, 0
+block0_y:
+.int 0, 1, 2, 3
+block1_x:
+.int 0, 1, 0, 1
+block1_y:
+.int 0, 0, 1, 1
+block2_x:
+.int 0, 0, 0, 1
+block2_y:
+.int 0, 1, 2, 2
+block3_x:
+.int 0, 1, 1, 2
+block3_y:
+.int 0, 0, 1, 1
+temp_x:
+.int 0
+temp_y:
+.int 0
+
+# represents current rotation of the block going down
+# can be 0 to 3
+current_rotation:
+.int 0
+# the current x of the block
+# i want to initailly set this in the function to start each 
+# new blcok
+current_x:
+.int 0
+current_y:
+.int 0
+# represents type of block currently 
+# can be 0 to 3
+current_block_type:
+.int 0
 score:
 .long 0
 level:
@@ -39,11 +75,25 @@ FRAME_COLOR_PAIR:
 .section .bss
 
 .section .text
+
+###################################
+# main_game
+###################################
 .globl main_game
 .type main_game, @function
 main_game:
 pushl %ebp           #save old base pointer
 movl  %esp, %ebp     #make stack pointer the base pointer
+
+# set the seed for generating random numbers later
+# c code
+# srand(time(NULL));
+pushl $0
+call time
+addl $4, %esp 
+pushl %eax 
+call srand
+addl $4, %esp 
 
 # get the screen size
 pushl stdscr
@@ -57,6 +107,153 @@ movl %eax, screen_x_size
 addl $4, %esp
 
 call draw_frame
+
+call main_game_loop
+
+movl %ebp, %esp      #restore the stack pointer
+popl %ebp            #restore the base pointer
+ret
+
+###################################
+# function draw_block
+# 
+###################################
+.type draw_block, @function
+draw_block:
+pushl %ebp           #save old base pointer
+movl  %esp, %ebp     #make stack pointer the base pointer
+
+.equ I, -4  # loop counter variable
+.equ START_X, -8  
+.equ START_Y, -12  
+.equ PRINT_X, -16  
+.equ PRINT_Y, -20 
+
+subl $20, %esp
+
+pushl BLOCK_COLOR_PAIR
+call attron
+addl $4, %esp
+
+# set up start_x_y
+# want the address at START_X and Y respectatively
+
+movl $block0_x, %eax
+movl %eax, START_X(%ebp)
+movl $block0_y, %eax
+movl %eax, START_Y(%ebp) 
+
+cmpl $0, current_block_type
+je end_set_up_start_x_y
+movl $block1_x, %eax
+movl %eax, START_X(%ebp)
+movl $block1_y, %eax
+movl %eax, START_Y(%ebp) 
+cmpl $1, current_block_type
+je end_set_up_start_x_y
+movl $block2_x, %eax
+movl %eax, START_X(%ebp)
+movl $block2_y, %eax 
+movl %eax, START_Y(%ebp) 
+cmpl $2, current_block_type
+je end_set_up_start_x_y
+movl $block3_x, %eax
+movl %eax, START_X(%ebp)
+movl $block3_y, %eax
+movl %eax, START_Y(%ebp) 
+end_set_up_start_x_y:
+
+movl $-1, I(%ebp)
+draw_loop:
+addl $1, I(%ebp)
+
+cmpl $4, I(%ebp)
+je end_draw_loop
+
+# START HERE seg fault. think it is at print statement
+
+	movl START_X(%ebp), %ecx
+	movl (%ecx), %ecx
+	# %ecx holds result
+	addl $4, START_X(%ebp)
+	# add current_x
+	addl current_x, %ecx  
+	movl %ecx, PRINT_X(%ebp)  	
+
+
+	movl START_Y(%ebp), %ecx
+	movl (%ecx), %ecx
+	# %ecx holds result
+	addl $4, START_Y(%ebp)
+	# add current_y
+	addl current_y, %ecx  
+	movl %ecx, PRINT_Y(%ebp)  	
+
+	pushl $x
+	pushl PRINT_X(%ebp)
+	pushl PRINT_Y(%ebp)
+	call mvprintw
+	addl $12, %esp
+	call refresh
+		
+jmp draw_loop
+end_draw_loop:
+
+pushl BLOCK_COLOR_PAIR
+call attroff
+addl $4, %esp
+
+movl %ebp, %esp      #restore the stack pointer
+popl %ebp            #restore the base pointer
+ret
+
+###################################
+# function new_block
+# whenever a new block is started at the top of the screen
+###################################
+.type new_block, @function
+new_block:
+pushl %ebp           #save old base pointer
+movl  %esp, %ebp     #make stack pointer the base pointer
+
+movl $8, current_x
+movl $1, current_y
+
+# generate a random number between 0 and 3 for the block type
+#current_block_type
+
+# c code
+#int result = rand() % 4
+call rand
+# FIXME $eax seems to have different value on calls 
+# most be a division problem
+movl $0, %edx
+movl $4, %edi
+divl %edi
+# remainder in %edx
+movl %edx, current_block_type
+
+movl %ebp, %esp      #restore the stack pointer
+popl %ebp            #restore the base pointer
+ret
+
+###################################
+# function main_game_loop
+###################################
+.type main_game_loop, @function
+main_game_loop:
+pushl %ebp           #save old base pointer
+movl  %esp, %ebp     #make stack pointer the base pointer
+
+call new_block
+main_loop:
+    # draw block
+	call draw_block
+
+	# sleep
+	pushl $1
+	call sleep
+jmp main_loop
 
 movl %ebp, %esp      #restore the stack pointer
 popl %ebp            #restore the base pointer
